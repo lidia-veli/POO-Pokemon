cfd04843d54bdedc0ce7dbe278392923aa5344cb
Lidia Velicia <lvelirui@myuax.com>
2023-03-31 22:54:17 +0200

    clase Pokemon

--- classes/pokemon.py
+++ classes/pokemon.py
@@ -33,7 +33,7 @@ this Python class.
 
 # Source packages.
 
-
+from weapon_type import WeaponType
 
 class Pokemon():
     """Python class to implement a basic version of a Pokemon of the game.
@@ -68,6 +68,88 @@ class Pokemon():
       >>> from weapon_type import WeaponType
       >>> obj_Pokemon = Pokemon(1, "Bulbasaur", WeaponType.PUNCH, 100, 7, 10)
     """
+
+    def __init__(self, pokemon_id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating):
+        # atributos privados
+        self.__pokemon_id = pokemon_id
+        self.__pokemon_name = pokemon_name
+        self.__weapon_type = weapon_type
+        self.__health_points = health_points
+        self.__attack_rating = attack_rating
+        self.__defense_rating = defense_rating
+
+        # verificamos que los parámetros de entrada son del tipo correcto y son válidos
+        if not isinstance(self.__pokemon_id, int):
+            raise TypeError("The parameter id must be an integer.")
+        # try:
+        #   self.num_id = int(pokemon_id)   es un entero
+        #   self.num_id not in lista_ids    no está en la lista de ids
+        # except:
+        #   raise TypeError("The parameter id must be a valid integer.")
+
+        if not isinstance(self.__pokemon_name, str):
+            raise TypeError("The parameter nombre_pokemon must be a string.")
+        if not isinstance(self.__weapon_type, WeaponType):
+            raise TypeError("The parameter tipo_arma must be a TipoArma.")
+        if not isinstance(self.__health_points, int) and self.__health_points not in range(1, 101):
+            raise TypeError("The parameter puntos_salud must be an integer between 1 and 100.")
+        if not isinstance(self.__attack_rating, int) and self.__attack_rating not in range(1, 11):
+            raise TypeError("The parameter indice_ataque must be an integer between 1 and 10.")
+        if not isinstance(self.__defense_rating, int) and self.__defense_rating not in range(1, 11):
+            raise TypeError("The parameter indice_defensa must be an integer between 1 and 10.")
+
+    def __del__(self): # eliminar la instancia de Pokemon de la lista global
+        pass
+
+    def __str__(self): # método que devuelve una cadena con la información del objeto
+        return "Pokemon ID " + str(self.__pokemon_id) + " with name " + self.__pokemon_name + " has as weapon " + self.__weapon_type + " and health " + str(self.__health_points)
+
+    # GETTERS y SETTERS
+    def get_pokemon_id(self): 
+        return self.__pokemon_id
+    def get_pokemon_name(self):
+        return self.__pokemon_name
+    def get_weapon_type(self):
+        return self.__weapon_type
+    def get_health_points(self):
+        return self.__health_points
+    def get_attack_rating(self):
+        return self.__attack_rating
+    def get_defense_rating(self):
+        return self.__defense_rating
+    # el único atributo modificable es el estado de salud del pokemon
+    def set_health_points(self, new_health_points):
+        self.__health_points = new_health_points
+    
+
+    def is_alive(self): # devuelve True si el pokemon sique vivo, esto es, tiene puntos de salud > 0
+        '''Método para saber si el Pokemon está vivo o no'''
+        if self.__health_points > 0:
+            return True
+        else:
+            return False
+
+    def fight_defense(self, damage_points):
+        '''Método que implementa la defensa del Pokémon de un golpe de otro Pokémon'''
+        if self.__defense_rating < damage_points: # si la defensa es menor que los puntos de daño, la salud se ve afectada
+            daño = damage_points - self.get_defense_rating() # calculamos el daño que recibirá el pokemon en base a sus puntos de defensa
+            self.set_health_points( self.get_health_points() - daño ) # actualizamos la salud del pokemon
+            return True
+        else:
+            return False
+    
+    def fight_attack(self, enemy):
+        '''Método que implementa el ataque del Pokémon usando un golpe sobre otro Pokémon.
+            Basado en el fight_defense del pokemon enemigo'''
+        # confirmamos que el enemigo es un objeto de la clase Pokemon
+        if not isinstance(enemy, Pokemon):
+            raise TypeError("The parameter enemy must be a Pokemon.")
+        else: # ya sabemos que el enemigo es un pokemon
+            if self.__attack_rating > enemy.get_defense_rating(): # si nuestro ataque es mayor que su defensa, hacemos daño al enemigo
+                enemy.fight_defense(self.__attack_rating) # el enemigo se defiende de nuestro ataque
+                return True
+            else:
+                return False
 
 
 
