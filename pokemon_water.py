d2690de522efe949feadb64a26d2611d6f656cde
Lidia Velicia <lvelirui@myuax.com>
2023-04-12 15:20:57 +0200

    corregir PokemonWater

--- pokemon_water.py
+++ pokemon_water.py
@@ -37,14 +37,14 @@ from pokemon import Pokemon
 from weapon_type import WeaponType
 
 
-class PokemonEarth(Pokemon):
+class PokemonWater(Pokemon):
     """Python class to implement a basic version of a Pokemon of the game.
 
     This Python class implements the basic version of a Pokemon of the game.
 
     Syntax
     ------
-      obj = PokemonEarth(id, pokemon_name, weapon_type, health_points,
+      obj = PokemonWater(id, pokemon_name, weapon_type, health_points,
                          attack_rating, defense_rating)
 
     Parameters
@@ -68,20 +68,20 @@ class PokemonEarth(Pokemon):
     -------
       >>> from pokemon import Pokemon
       >>> from weapon_type import WeaponType
-      >>> obj_Pokemon = PokemonEarth(1, "Diglett", WeaponType.PUNCH, 100, 7, 10)
+      >>> obj_Pokemon = PokemonWater(1, "Squirtle", WeaponType.PUNCH, 100, 7, 10)
     """
 
     def __init__(self, pokemon_id, pokemon_name, weapon_type, health_points, attack_rating, defense_rating):
-        # importamos el constructor de la clase madre
-        # (con un valor de 1 para el parámetro defense_rating, para que no nos salte el error, que ya lo modificamos después)
-        super().__init__(pokemon_id, pokemon_name, weapon_type, health_points, attack_rating, 1)
+        # importamos el constructor de la clase madre 
+        # (con un valor de 1 para el parámetro attack_rating, para que no nos salte el error, que ya lo modificamos después)
+        super().__init__(pokemon_id, pokemon_name, weapon_type, health_points, 1, defense_rating)
 
-        # y cambiamos las condiciones para el parámetro defense_rating: rango 11-20
-        if defense_rating in range(11,21):
-            self.set_defense_rating(defense_rating)
+        # y cambiamos las condiciones para el parámetro attack_rating: rango 11-20
+        if attack_rating in range(11,21):
+            self.set_attack_rating(attack_rating)
         else:
-            raise ValueError("The parameter defense_rating must be between 11 and 20.")
-
+            raise ValueError("The parameter attack_rating must be between 11 and 20.")
+        
 
 def main():
     """Function main of the module.
@@ -109,9 +109,9 @@ def main():
     print("=================================================================.")
     print("Test Case 1: Create a Pokemon.")
     print("=================================================================.")
-    pokemon_1 = PokemonEarth(1, "Diglett", WeaponType.HEADBUTT, 100, 8, 15)
+    pokemon_1 = PokemonWater(1, "Squirtle", WeaponType.HEADBUTT, 100, 12, 8)
 
-    if pokemon_1.get_pokemon_name() == "Diglett":
+    if pokemon_1.get_pokemon_name() == "Squirtle":
         print("Test PASS. The parameter pokemon_name has been correctly set.")
     else:
         print("Test FAIL. Check the method __init__().")
@@ -126,12 +126,12 @@ def main():
     else:
         print("Test FAIL. Check the method __init__().")
 
-    if pokemon_1.get_attack_rating() == 8:
+    if pokemon_1.get_attack_rating() == 12:
         print("Test PASS. The parameter attack_rating has been correctly set.")
     else:
         print("Test FAIL. Check the method __init__().")
 
-    if pokemon_1.get_defense_rating() == 15:
+    if pokemon_1.get_defense_rating() == 8:
         print("Test PASS. The parameter defense_rating has been correctly set.")
     else:
         print("Test FAIL. Check the method __init__().")
@@ -140,9 +140,9 @@ def main():
     print("=================================================================.")
     print("Test Case 2: Human-readable format of the object.")
     print("=================================================================.")
-    pokemon_2 = PokemonEarth(7, "Diglett", WeaponType.HEADBUTT, 100, 7, 12)
+    pokemon_2 = PokemonWater(7, "Squirtle", WeaponType.HEADBUTT, 100,15, 7)
 
-    if str(pokemon_2) == "Pokemon ID 7 with name Diglett has as weapon HEADBUTT and health 100":
+    if str(pokemon_2) == "Pokemon ID 7 with name Squirtle has as weapon HEADBUTT and health 100":
         print("Test PASS. The human-readable format of the object has been implemented correctly.")
     else:
         print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
@@ -151,7 +151,7 @@ def main():
     print("=================================================================.")
     print("Test Case 3: Pokemon alive?¿?.")
     print("=================================================================.")
-    pokemon_3 = PokemonEarth(3, "Diglett", WeaponType.KICK, 97, 8, 15)
+    pokemon_3 = PokemonWater(3, "Squirtle", WeaponType.KICK, 97, 15, 8)
 
     if pokemon_3.is_alive():
         pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
@@ -167,11 +167,11 @@ def main():
     print("=================================================================.")
     print("Test Case 4: Check the defense during a Fight.")
     print("=================================================================.")
-    pokemon_4 = PokemonEarth(4, "Diglett", WeaponType.ELBOW, 93, 9, 11)
+    pokemon_4 = PokemonWater(4, "Squirtle", WeaponType.ELBOW, 93, 11, 9)
 
     pokemon_4.fight_defense(70)
 
-    if pokemon_4.get_health_points() == 34:
+    if pokemon_4.get_health_points() == 32:
         print("Test PASS. The method fight_defense() has been implemented correctly.")
     else:
         print("Test FAIL. Check the method fight_defense().")
@@ -180,13 +180,13 @@ def main():
     print("=================================================================.")
     print("Test Case 5: Check the attack during a Fight.")
     print("=================================================================.")
-    pokemon_5 = PokemonEarth(5, "Diglett", WeaponType.PUNCH, 99, 10, 20)
-    pokemon_6 = PokemonEarth(6, "Diglett", WeaponType.PUNCH, 99, 9, 18)
+    pokemon_5 = PokemonWater(5, "Squirtle", WeaponType.PUNCH, 99, 20, 10)
+    pokemon_6 = PokemonWater(6, "Squirtle", WeaponType.PUNCH, 99, 18, 9)
 
     pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
 
     if pokemon_was_hit:
-        if pokemon_6.get_health_points() == 97:
+        if pokemon_6.get_health_points() == 88:
             print("Test PASS. The method fight_attack() has been implemented correctly.")
         else:
             print("Test FAIL. Check the method fight_attack().")
