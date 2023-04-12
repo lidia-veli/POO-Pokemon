596ef957da8c06e04a96b6df16e8486b9b217be7
Lidia Velicia <lvelirui@myuax.com>
2023-04-12 14:33:23 +0200

    corregir PokemonAir

---
+++ pokemon_air.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+This Python module contains not only the class Pokemon, but also the test of
+this Python class.
+
+@contents :  This module contains not only a single Python class, but also the
+             test cases to probe its functionality.
+@project :  N/A
+@program :  N/A
+@file :  pokemon.py
+@author :  Antonio Artes Garcia (antonio.artesgarcia@ceu.es)
+           Francisco Hernando Gallego (francisco.hernandogallego@ceu.es)
+           Ruben Juarez Cadiz (ruben.juarezcadiz@ceu.es)
+
+@version :  0.0.1, 08 November 2021
+@information :  The Zen of Python
+                  https://www.python.org/dev/peps/pep-0020/
+                Style Guide for Python Code
+                  https://www.python.org/dev/peps/pep-0008/
+                Example NumPy Style Python Docstrings
+                  http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_numpy.html
+                doctest – Testing through documentation
+                  https://pymotw.com/2/doctest/
+
+@copyright :  Copyright 2021 GNU AFFERO GENERAL PUBLIC.
+              All rights are reserved. Reproduction in whole or in part is
+              prohibited without the written consent of the copyright owner.
+"""
+
+
+# Source packages.
+
+from random import randint
+from pokemon import Pokemon
+from weapon_type import WeaponType
+
+class PokemonAir(Pokemon):
+    """Python class to implement a basic version of a Pokemon of the game.
+
+    This Python class implements the basic version of a Pokemon of the game.
+
+    Syntax
+    ------
+      obj = PokemonAir(id, pokemon_name, weapon_type, health_points,
+                         attack_rating, defense_rating)
+
+    Parameters
+    ----------
+      [in] id ID of the Pokemon.
+      [in] pokemon_name Name of the Pokemon.
+      [in] weapon_type Type of weapon that carries out the Pokemon.
+      [in] health_points Points of health that the Pokemon has.
+      [in] attack_rating Attack rating of the Pokemon.
+      [in] defense_rating Defense rating of the Pokemon.
+
+    Returns
+    -------
+      obj Python object output parameter that represents an instance
+          of the class Pokemon.
+
+    Attributes
+    ----------
+
+    Example
+    -------
+      >>> from pokemon import Pokemon
+      >>> from weapon_type import WeaponType
+      >>> obj_Pokemon = PokemonEarth(1, "Pidgey", WeaponType.PUNCH, 100, 7, 10)
+    """
+
+    def fight_defense(self, damage_points):
+        '''Método que implementa la defensa del Pokemon tipo Tierra.
+            Se carcateriza por que hay un 50% de probabilidad de que el ataque no le afecte'''
+        if randint(0,1) == 1:  # el ataque le afecta
+            damage = damage_points - self.get_defense_rating()
+            self.set_health_points( self.get_health_points() - damage )
+            return True
+        else:  # el ataque no le afecta
+            return False
+    
+    # hay también que modificar el método fight_attack porque ahora que el ataque se efectúe no depende sólo
+    # de la diferencia entre el ataque y la defensa, sino también de la probabilidad de que el ataque le afecte
+    # al pokemon enemigo
+    
+    def fight_attack(self, enemy):
+        '''Método que implementa el ataque del Pokémon usando un golpe sobre otro Pokémon.
+            Basado en el fight_defense del pokemon enemigo'''
+        if not isinstance(enemy, Pokemon):  # nos aseguramos de que el enemigo es un objeto de la clase Pokemon
+            raise TypeError("The parameter enemy must be a Pokemon.")
+        else:
+            if self.get_attack_rating() >= enemy.get_defense_rating(): # si nuestro ataque es mayor que su defensa, podemos atacarle
+                # si podemos atacarle    
+                if enemy.fight_defense( self.get_attack_rating() ) is True:
+                    # el enemigo ha recibido daño
+                    return True
+                else: # el enemigo no recibe daño
+                    return False
+            else:
+                return False # no hemos podido atacar
+
+
+def main():
+    """Function main of the module.
+
+    The function main of this module is used to test the Class that is described
+    in this module.
+
+    Syntax
+    ------
+      [ ] = main()
+
+    Parameters
+    ----------
+      Null .
+
+    Returns
+    -------
+      Null .
+
+    Example
+    -------
+      >>> main()
+    """
+
+    print("=================================================================.")
+    print("Test Case 1: Create a Pokemon.")
+    print("=================================================================.")
+    pokemon_1 = PokemonAir(1, "Pidgey", WeaponType.HEADBUTT, 100, 8, 7)
+
+    if pokemon_1.get_pokemon_name() == "Pidgey":
+        print("Test PASS. The parameter pokemon_name has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_weapon_type().name == "HEADBUTT":
+        print("Test PASS. The parameter weapon_type has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_health_points() == 100:
+        print("Test PASS. The parameter health_points has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_attack_rating() == 8:
+        print("Test PASS. The parameter attack_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_defense_rating() == 7:
+        print("Test PASS. The parameter defense_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+
+    print("=================================================================.")
+    print("Test Case 2: Human-readable format of the object.")
+    print("=================================================================.")
+    pokemon_2 = PokemonAir(7, "Pidgey", WeaponType.HEADBUTT, 100, 7, 6)
+
+    if str(pokemon_2) == "Pokemon ID 7 with name Pidgey has as weapon HEADBUTT and health 100":
+        print("Test PASS. The human-readable format of the object has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
+
+
+    print("=================================================================.")
+    print("Test Case 3: Pokemon alive?¿?.")
+    print("=================================================================.")
+    pokemon_3 = PokemonAir(3, "Pidgey", WeaponType.KICK, 97, 8, 7)
+
+    if pokemon_3.is_alive():
+        pokemon_was_hit = pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
+
+        if pokemon_was_hit:
+            if not pokemon_3.is_alive():
+                print("Test PASS. The method is_alive() has been implemented correctly.")
+            else:
+                print("Test FAIL. Check the method is_alive().")
+        else:
+            if pokemon_3.is_alive():
+                print("Test PASS. The method is_alive() has been implemented correctly.")
+            else:
+                print("Test FAIL. Check the method is_alive().")
+            
+    else:
+        print("Test FAIL. Check the method is_alive().")
+
+
+    print("=================================================================.")
+    print("Test Case 4: Check the defense during a Fight.")
+    print("=================================================================.")
+    pokemon_4 = PokemonAir(4, "Pidgey", WeaponType.ELBOW, 93, 9, 5)
+
+    pokemon_was_hit = pokemon_4.fight_defense(70)
+
+    if pokemon_was_hit:
+        if pokemon_4.get_health_points() == 28:
+            print("Test PASS. The method fight_defense() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_defense().")
+    else:
+        if pokemon_4.get_health_points() == 93:
+            print("Test PASS. The method fight_defense() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_defense().")
+
+
+    print("=================================================================.")
+    print("Test Case 5: Check the attack during a Fight.")
+    print("=================================================================.")
+    pokemon_5 = PokemonAir(5, "Pidgey", WeaponType.PUNCH, 99, 10, 8)
+    pokemon_6 = PokemonAir(6, "Pidgey", WeaponType.PUNCH, 99, 9, 6)
+
+    pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
+
+    if pokemon_was_hit:
+        if pokemon_6.get_health_points() == 95:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+    else:
+        if pokemon_6.get_health_points() == 99:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+
+
+
+# Checking whether this module is executed just itself alone.
+if __name__ == "__main__":
+    main()
+
+
+# EOF
